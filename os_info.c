bff9856d24f42feed04cd27dff11e67c31ecbae3
hugocornellierunb <113365651+hugocornellierunb@users.noreply.github.com>
2022-09-19 13:59:16 -0300

    Update os_info.c

--- os_info.c
+++ os_info.c
@@ -1,10 +1,10 @@
 /*
   os_info.c
   COMP 3430 Operating Systems
-  
-  An example of using the /proc pseudo file system in Linux. This 
+
+  An example of using the /proc pseudo file system in Linux. This
   program reads and prints OS info from /proc/sys/kernel.
-  
+
   To compile: gcc os_info.c
 */
 #include <stdio.h>
@@ -13,35 +13,43 @@
 #define BUFSIZE 81
 
 int main (){
-  FILE* fp;
-  char info[BUFSIZE];
-  
-  printf("OS information\n");
-  printf("==============\n");
-  fp = fopen ("/proc/sys/kernel/ostype", "r");
-  if(fp == NULL)
-    printf("file open failed\n");
-  else{
-    fgets(info, BUFSIZE, fp);
-    fclose (fp);
-    printf("     OS: %s", info);
-   }
-  fp = fopen ("/proc/sys/kernel/version", "r");
-  if(fp == NULL)
-    printf("file open failed\n");
-  else{
-    fgets(info, BUFSIZE, fp);
-    fclose (fp);
-    printf("Version: %s", info);
-   }
-  fp = fopen ("/proc/sys/kernel/osrelease", "r");
-  if(fp == NULL)
-    printf("file open failed\n");
-  else{
-    fgets(info, BUFSIZE, fp);
-    fclose (fp);
-    printf("Release: %s", info);
-   }
-  
-  return 0;
+    FILE* fp;
+    char info[BUFSIZE];
+
+    printf("OS information\n");
+    printf("==============\n");
+    fp = fopen ("/proc/sys/kernel/ostype", "r");
+    if(fp == NULL)
+        printf("file open failed\n");
+    else{
+        fgets(info, BUFSIZE, fp);
+        fclose (fp);
+        printf("     OS: %s", info);
+    }
+    fp = fopen ("/proc/sys/kernel/version", "r");
+    if(fp == NULL)
+        printf("file open failed\n");
+    else{
+        fgets(info, BUFSIZE, fp);
+        fclose (fp);
+        printf("Version: %s", info);
+    }
+    fp = fopen ("/proc/sys/kernel/osrelease", "r");
+    if(fp == NULL)
+        printf("file open failed\n");
+    else{
+        fgets(info, BUFSIZE, fp);
+        fclose (fp);
+        printf("Release: %s", info);
+    }
+    fp = fopen ("/proc/sys/kernel/hostname", "r");
+    if(fp == NULL)
+        printf("file open failed\n");
+    else{
+        fgets(info, BUFSIZE, fp);
+        fclose (fp);
+        printf("Hostname: %s", info);
+    }
+
+    return 0;
 }// main
